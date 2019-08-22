dc3869fa8fee09f62733717f6337c0d15f7ec222
jokthefoo <alexkofford@gmail.com>
2019-08-22 10:27:21 -0600

    Made firstperson

---
+++ Source/Swordplay/SwordplayGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwordplayGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASwordplayGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASwordplayGameMode();
+};
+
+
+
---
+++ Source/Swordplay/SwordplayHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SwordplayHUD.generated.h"
+
+UCLASS()
+class ASwordplayHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ASwordplayHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
